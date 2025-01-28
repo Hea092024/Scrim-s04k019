0d5d9c8c306356ae1fee9650e4dc74410b0dc705
Hea092024 <Hea092024@gmail.com>
2025-01-28 12:17:09 +0100

    made reset to work on both home + away side

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ function incrementHomeScore(points) {
     homeScoreElement.textContent = currentScore;
 }
 
-function reset() {
-    const homeScoreElement = document.getElementById("home-score");
-    homeScoreElement.textContent = 0;
+function reset(team) {
+    const ScoreElement = document.getElementById(`${team}-score`);
+    ScoreElement.textContent = 0;
+}
+
+function incrementAwayScore (points) {
+    const AwayScoreElement = document.getElementById("away-score");
+    let currentScore = parseInt(AwayScoreElement.textContent);
+    currentScore += points;
+    AwayScoreElement.textContent = currentScore;
 }
