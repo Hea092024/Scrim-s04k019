eeeb631cbc76a8ca5c1a32f25f49ce9f7f0a49d9
Hea092024 <Hea092024@gmail.com>
2025-01-28 11:43:15 +0100

    added funtions to the add buttons

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function incrementHomeScore(points) {
     currentScore += points;
     homeScoreElement.textContent = currentScore;
 }
+
+const homeButtons = document.querySelectorAll("#home .score-btn");
+homeButtons.forEach(button => { 
+    button.addEventListener("click", (event) => {
+        const points = parseInt(event.target.textContent.replace("+", ""));
+        incrementHomeScore(points);
+    });
+})
